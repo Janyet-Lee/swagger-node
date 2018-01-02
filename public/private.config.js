81742386e7a0338456fddc6bb0d7add3d7cb9f04
Lee <419452043@qq.com>
2018-01-02 19:24:46 +0800

    初始化

---
+++ public/private.config.js
@@ -0,0 +1,10 @@
+  var configArray = [
+    {
+      name: '测试接口文档1', // 项目名称，必须值
+      fileName: 'api1.json', // 接口文件全名，必须值
+    },
+    {
+      name: '测试接口文档2',
+      fileName: 'api2.json',
+    }
+  ]
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var app = express();
+
+var params
+
+app.get('/swagger/api/:fileName', function(req, res, next) {
+  params = req.params
+  fs.readFile(__dirname + '/public/index.html', {flag: 'r+', encoding: 'utf8'}, function (err, data) {
+    if(err) {
+     console.error(err);
+     return;
+    }
+    var result = data.replace(/.\/api\/(.+)json/, './api/' + params.fileName)
+    fs.writeFile(__dirname + '/public/index.html', result, {flag: 'w', encoding: 'utf8'}, function(err) {
+      if (err) {
+        console.error(err)
+        return
+      }
+      next()
+    })
+  });
+})
+
+app.use('/swagger/api/:name',express.static('public'));
+app.use('/swagger',express.static('public'));
+
+app.get('/*', function(req, res, next) {
+  res.redirect('/swagger/home.html')
+})
+ 
+var server = app.listen(3200, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
