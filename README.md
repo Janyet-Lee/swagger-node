928f48a398b22ebf646f57b0b2a209f4d7d13be6
Lee <419452043@qq.com>
2018-01-09 15:44:53 +0800

    细节调整

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## 启动方式
 - 复制private.config.example.js并重命名为private.config.js
 - npm install 安装项目依赖
-- npm start 启动服务器进程
+- npm run build 使用browserify生成浏览器支持的服务端js模块(注意：每次添加新的接口json文件时候请跑这一步)
+- npm run start 启动服务器进程
 
 ## 使用方式
 
 - 导出的json接口文件请放在public目录下api目录里面
-- 由于浏览器的模块调用方式和服务器有所不同，因此服务器启动相关的配置存放在项目目录下private.config.js里
-- api文档的配置在public目录下api-config.js内,后期再尝试把两者统一
+- 服务器启动相关的配置和api文档的配置存放在项目目录下private.config.js里,接口文档的命名名称请遵循api-xxx.json的原则，比如停车场项目：api-parking-lot.json
--- public/apiConfig.js
+++ public/apiConfig.js
@@ -4,59 +4,59 @@ module.exports = {
   configArray: [
     {
       name: '停车场项目', // 项目名称，必须值
-      fileName: 'api1.json', // 接口文件全名，必须值
+      fileName: 'api-wechat.json', // 接口文件全名，必须值
     },
     {
       name: '金融贷项目',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '新周刊移动端',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '新周刊管理后台',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '车尚二期',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '车尚一期',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '投资广东管理后台',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '新周刊PC端',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '广东重工监理管理后台',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '资料管理系统',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '智慧班牌微信端',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '智慧班牌管理后台',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '智慧班牌PC端',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     },
     {
       name: '测试接口测试接口文档2文档2',
-      fileName: 'api2.json',
+      fileName: 'api-admin.json',
     }
   ]
 }
